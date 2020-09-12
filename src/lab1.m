2cc581bb657164b60d7bf00f6aa995497593739a
Alex Tacescu <alextac98@gmail.com>
2020-09-12 19:44:44 -0400

    Add initial files

--- src/lab1.m
+++ src/lab1.m
@@ -59,8 +59,8 @@ try
       packet(1) = 1000;%one second time
       packet(2) = 0;%linear interpolation
       packet(3) = k;
-      packet(4) = 0;% Second link to 0
-      packet(5) = 0;% Third link to 0
+      packet(4) = k;% Second link to 0
+      packet(5) = k;% Third link to 0
 
       % Send packet to the server and get the response      
       %pp.write sends a 15 float packet to the micro controller
